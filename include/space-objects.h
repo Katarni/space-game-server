463255548a38109df622b5c3c82c2246084adee9
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2025-03-30 15:50:16 +0300

    some methods added to space objects

--- include/space-objects.h
+++ include/space-objects.h
@@ -2,29 +2,59 @@
 
 #include "include.h"
 
+
+struct Point {
+   int32_t x, y;
+
+   Point() = default;
+   Point(int32_t x, int32_t y) {
+      this->x = x;
+      this->y = y;
+   }
+};
+
+
 class StarSystem;
 
-class Planet {
- public:
-    enum class Resource {
-        Metal, Gold, Spice
-    };
 
-    explicit Planet(StarSystem* system);
-    Planet(int8_t owner, StarSystem* system);
+class Planet {
+public:
+   enum class Resource {
+      Metal, Gold, Spice
+   };
+
+   explicit Planet(StarSystem* system) : star_system_(system), owner_(0),
+                                          resource_(Planet::Resource(random() % 3)),
+                                          resource_cnt_(random() % 8 + 1) {}
+
+   Planet(int8_t owner, StarSystem* system) : star_system_(system), owner_(owner),
+                                             resource_(Planet::Resource(random() % 3)),
+                                             resource_cnt_(random() % 8 + 1) {}
+
+   inline void setOwner(int8_t owner);
+   void capture(int8_t new_owner);
 
  private:
-    StarSystem* star_system_;
-    Resource resource_;
-    int8_t owner_;
+   StarSystem* star_system_;
+   Resource resource_;
+   int8_t owner_, radius_, speed_, resource_cnt_;
 };
+
 
 class StarSystem {
  public:
-    explicit StarSystem(int8_t planet_cnt);
-    ~StarSystem();
+   explicit StarSystem(int64_t id, int8_t planet_cnt,  const Point& pos);
+   ~StarSystem();
+
+   inline void addTerritoryPoint(const Point& point);
+
+   inline void setOwner(int8_t owner);
+   void planetCaptured(int8_t capture_lvl_edit_);
 
  private:
-    int8_t owner_;
-    std::vector<Planet*> planets_;
+   int64_t id_;
+   int8_t owner_, capture_lvl_;
+   Point pos_;
+   std::vector<Planet*> planets_;
+   std::vector<Point> territory_;
 };
--- source/space-objects.cpp
+++ source/space-objects.cpp
@@ -1,14 +1,19 @@
 #include "../include/space-objects.h"
 
-Planet::Planet(StarSystem* system) : star_system_(system),
-                                        owner_(0),
-                                        resource_(Planet::Resource(random() % 3)) {}
+void Planet::setOwner(int8_t owner) {
+    owner_ = owner;
+}
 
-Planet::Planet(int8_t owner, StarSystem* system) : star_system_(system),
-                                                    owner_(owner),
-                                                    resource_(Planet::Resource(random() % 3)) {}
+void Planet::capture(int8_t new_owner) {
+    int8_t lvl_edit = new_owner;
+    if (owner_ != 0) {
+        lvl_edit *= 2;
+    }
+    setOwner(new_owner);
+    star_system_->planetCaptured(lvl_edit);
+}
 
-StarSystem::StarSystem(int8_t planet_cnt) : owner_(0) {
+StarSystem::StarSystem(int64_t id, int8_t planet_cnt, const Point& pos) : id_(id), owner_(0), capture_lvl_(0), pos_(pos) {
     planets_.resize(planet_cnt, nullptr);
     for (auto& planet : planets_) {
         planet = new Planet(this);
@@ -19,4 +24,20 @@ StarSystem::~StarSystem() {
     for (auto& planet : planets_) {
         delete planet;
     }
+}
+
+void StarSystem::addTerritoryPoint(const Point& point) {
+    territory_.push_back(point);
+}
+
+void StarSystem::setOwner(int8_t owner) {
+    owner_ = owner;
+    capture_lvl_ = planets_.size() * owner_;
+    for (auto planet : planets_) {
+        planet->setOwner(owner);
+    }
+}
+
+void StarSystem::planetCaptured(int8_t capture_lvl_edit_) {
+    capture_lvl_ += capture_lvl_edit_;
 }
